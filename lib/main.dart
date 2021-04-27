7aee626000cead014ff29800d8665df2aa8a451b
kolamala <mohan.kolamala@gmail.com>
2021-04-27 11:17:36 -0700

    my first flutter app hello world

---
+++ lib/main.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HelloWorldApp());
+}
+
+class HelloWorldApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black12,
+          title:Text(
+            "Hello World APP" ,
+            textDirection: TextDirection.ltr,
+          ),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 200,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.black26
+                  ),
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(20.0),
+                  child: Text(
+                    "Hello World!",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.black26
+                        ),
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.all(20.0),
+                        child: Text(
+                          "Hello World!",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.black26
+                        ),
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.all(20.0),
+                        child: Text(
+                          "Hello World!",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.black26
+                        ),
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.all(20.0),
+                        child: Text(
+                          "Hello World!",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.black26
+                        ),
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.all(20.0),
+                        child: Text(
+                          "Hello World!",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 200,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.black26
+                  ),
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(20.0),
+                  child: Text(
+                    "Hello World!",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 200,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.black26
+                  ),
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.all(20.0),
+                  child: Text(
+                    "Hello World!",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
